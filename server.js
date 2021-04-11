588cc8212ef92746920f0aeade0d49ac6da68460
Sam <samjbaker1234@gmail.com>
2021-04-11 15:07:26 +0100

    db.js file added - MongoDB connection working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+const db = require('./db');
 
 // Get our API routes
 const api = require('./server/routes/api');
