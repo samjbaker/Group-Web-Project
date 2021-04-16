1bc705a542782265ff058b9426c45705d8d08cd9
abbyex <abbyexon@gmail.com>
2021-04-16 15:52:33 -0300

    adding merged files

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-const db = require('./db');
 
 // Get our API routes
 const api = require('./server/routes/api');
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'testsite';
+export class AppComponent implements OnInit {
+  public name = ['default'];
+dataList: any = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.retrieveData();
+  }
+
+  retrieveData() {
+    this.dataService.getAll().subscribe(
+        data => {
+          this.dataList = data;
+          this.name = this.dataList.name;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
 }
