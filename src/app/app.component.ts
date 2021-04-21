65a324b23005a9a55bcffe36d157c15b53ee9fd9
Sam <samjbaker1234@gmail.com>
2021-04-21 14:58:33 +0100

    merging with abby_dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'testsite';
+export class AppComponent implements OnInit {
+  public post_text = ['default'];
+dataList: any = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.retrieveData();
+  }
+
+  retrieveData() {
+    this.dataService.getAll().subscribe(
+        Sentence => {
+          this.dataList = Sentence;
+          this.post_text = this.dataList.post_text;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
 }
