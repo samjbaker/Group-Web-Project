218642e321db0bae8e33db287a6006ec61b95277
Sam <samjbaker1234@gmail.com>
2021-04-21 17:47:30 +0100

    removing un-needed files

--- dbdata/mongo-seed.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-mongoimport --host=127.0.0.1 --port 27017 -u "testuser" -p "testpass" --authenticationDatabase admin --db test --collection post --drop --file ./data/db/test.json --jsonArray
