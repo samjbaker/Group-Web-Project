dff882d81fb8dfad8c86ad820f7690c2e226f6e6
abbyex <abbyexon@gmail.com>
2021-04-21 11:18:17 -0300

    adding updated api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const data = require('./test.json')
+const posts = require('../../models/posts_model');
+//const data = require('./data.json')
 
-/* GET api listing. */
-router.get('/', function (req, res) {
-  res.header("Content-Type",'application/json');
-  res.send(JSON.stringify(data));
-})
+// Get a sentence from the database 
+router.get('/', function (req, res, next) {
+  Posts.aggregate([{ $sample: {size: 1}}]).then(function(posts){
+    res.send(posts);
+  });
+});
+
+// Add a new sentence to the database
+router.post('/', function (req, res, next) {
+  Posts.create(req.body).then(function(posts){ // creates & saves to DB
+    res.send(posts);
+  }).catch(next);
+});
+
+// Update a sentence in the database
+router.put('/:id', function (req, res, next) {
+  Posts.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(posts){
+    Posts.findOne({_id: req.params.id}).then(function(posts){
+    res.send(posts);
+    });
+  });
+});
+
+// Delete a sentence from the database
+router.delete('/:id', function (req, res, next) {
+  Posts.findByIdAndRemove({_id: req.params.id}).then(function(posts){
+  res.send(posts);
+  });
+});
 
 module.exports = router;
